b2d956b7a8fbe29dd4d3065f4d189edcacccf7a9
imyaman <imyaman@netscape.net>
2025-04-20 00:40:03 +0000

    3

--- app.pl
+++ app.pl
@@ -1,54 +1,40 @@
 #!/usr/bin/env perl
- 
+
+use Dancer2;
+use Data::Dumper;
 use JSON qw(from_json);
 use URI::Encode qw(uri_decode);
-use Data::Dumper;
-use Dancer2;
 
 get '/' => sub {
-   my $err;
-   template 'index.tt', {
-       'err' => $err,
-   };
+    template 'index.tt';
 };
 
 get '/envs' => sub {
-  my $self = shift;
-  my ($key, $envstring);
-  for $key (keys %ENV){
-    $envstring .= "$key : $ENV{$key}<br>\n";
-  }
-  return $envstring;
+    my $env_string = "";
+    for my $key (keys %ENV) {
+        $env_string .= "$key : $ENV{$key}<br>\n";
+    }
+    return $env_string;
 };
 
 post '/github' => sub {
-  my $self = shift;
-  my ($mybody, $mypayload, $mypayloadjson);
-#  for $key (keys %ENV){
-#    $envstring .= "$key : $ENV{$key}<br>\n";
-#  }
-#  return $envstring;
+    my $request_body = request->body;
+    my $payload = $request_body;
 
-  $mybody = request -> body;
-  $mypayload = $mybody;
-  $mypayload =~ s/payload=//;
-  $mypayload = uri_decode($mypayload); 
-  $mypayloadjson = from_json($mypayload);
+    $payload =~ s/payload=//;
+    $payload = uri_decode($payload);
+    my $payload_json = from_json($payload);
 
-#  debug Dumper($mypayloadjson);
-#  debug $mypayloadjson->{pusher};
-  if($mypayloadjson->{pusher}){
-    # do deploy
-    mydeploy();
-  }else{
-    # do not deploy, check again
-   
-  }
-  return '{"result":"OK"}';
+    if ($payload_json->{pusher}) {
+        # Do deployment
+        _deploy();
+    }
+
+    return '{"result":"OK"}';
 };
 
-sub mydeploy {
-  debug "I'll do deploy";
+sub _deploy {
+    debug "Deploying...";
 }
 
 dance;
